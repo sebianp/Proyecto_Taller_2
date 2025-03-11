4bf29ddf86057a9cc1993d213469aee7c3e09865
Sebastian Prado <sebianp@gmail.com>
2025-03-10 21:58:46 -0300

    Actualizacion en la capa entidades, se creo la clase CATEGORIA definiendo así esta entidad

---
+++ Entidades/Categoria.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Entidades
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
